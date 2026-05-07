ee70196483badaba5a1f6ffd941dd23061299b66
Pablo <pfeijo34@gmail.com>
2026-05-07 12:18:54 -0500

    Trabajo Grupal v-8

--- src/main/java/ec/edu/ups/bibliotecatrabajogrupal/Objeto/Autor.java
+++ src/main/java/ec/edu/ups/bibliotecatrabajogrupal/Objeto/Autor.java
@@ -1,7 +1,6 @@
 package ec.edu.ups.bibliotecatrabajogrupal.Objeto;
-public class Autor {
-    private String nombre;
-    private String apellido;
+import java.util.Date;
+public class Autor extends Persona {
     private String seudonimo;
     private String nacionalidad;
     private String generoAutor;
@@ -9,29 +8,17 @@ public class Autor {
     public Autor(){
         
     }
-    public Autor(String nombre,String apellido,String seudonimo,String nacionalidad,String generoAutor){
-        this.nombre = nombre;
-        this.apellido =apellido;
-        this.seudonimo =seudonimo;
+    
+    public Autor(String nombre,String cedula, Date fechaNac,String apellido, String seudonimo,
+            String nacionalidad ,String generoAutor){
+        super(nombre,cedula,fechaNac,apellido);
+        this.seudonimo = seudonimo;
         this.nacionalidad = nacionalidad;
         this.generoAutor = generoAutor;
     }
     
-    public String getNombre(){
-        return nombre;
-    }
     
-    public void setNombre(String nombre){
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
+    
 
     public String getSeudonimo() {
         return seudonimo;
@@ -61,8 +48,6 @@ public class Autor {
     @Override
      public String toString(){
         String resultado = "\n";
-        resultado += "Nombre Autor: " + nombre + "\n";
-        resultado += "Apellido Autor: " + apellido + "\n";
         resultado += "Apodo: " + seudonimo + "\n";
         resultado += "Nacionalidad: " + nacionalidad + "\n";
         resultado += "Genero del Autor: " + generoAutor;
